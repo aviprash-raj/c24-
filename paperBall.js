4b3f0854185dbea9e17c14cc26af50636bde6609
AVIPRASH RAJ <73380723+aviprash-raj@users.noreply.github.com>
2021-02-20 20:32:47 +0530

    Add files via upload

---
+++ paperBall.js
@@ -0,0 +1,21 @@
+class Paper{
+    constructor(x,y,Radius){
+    var option ={
+        isStatic:false,
+      restitution:0.3,
+      friction:0.8,
+       density:1.5
+    }
+    this.body=Bodies.circle(x,y,Radius,option);
+    this.Radius=Radius;
+      World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position
+        fill("blue");
+        ellipseMode(CENTER);
+        ellipse(pos.x,pos.y,this.Radius);
+    
+    }
+    
+    }
